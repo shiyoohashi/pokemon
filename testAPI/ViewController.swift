be3186fbbb5857a14812183798926358a937b14b
shiyoohashi <alicehecate666@gmail.com>
2022-06-22 20:01:22 +0900

    Initial Commit

---
+++ testAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testAPI
+//
+//  Created by shiyo_ohashi on 2022/06/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
